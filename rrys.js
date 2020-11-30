785ed299741205bf83ff45923675dc7d1b83b1e7
sictang <63572138+sictang@users.noreply.github.com>
2020-11-30 09:48:57 +0800

    Update rrys.js

--- rrys.js
+++ rrys.js
@@ -1,6 +1,6 @@
 const url = "http://www.rrys2020.com/user/login/getCurUserTopInfo";
 const method = "POST";
-const headers = {Cookie:'GINFO=uid%3D10395821%26nickname%3Dsiaanga%26group_id%3D1%26avatar_t%3Dhttp%3A%2F%2Fimage.jstucdn.com%2Fftp%2Favatar%2Ff_noavatar_t.gif%26main_group_id%3D0%26common_group_id%3D52; GKEY=b283b63e753f6d2dbf3f5a32680d4f87'};
+const headers = {};
 const data = {"info": "abc"};
 
 const myRequest = {
@@ -17,7 +17,7 @@ $task.fetch(myRequest).then(response => {
     const cont_login = body.data.usercount.cont_login
     console.log(nickname);
     console.log(cont_login);
-    $notify("Title", "Subtitle", nickname); // Success!
+    $notify("Title", , nickname,"签到天数:"+cont_login); // Success!
     $done();
 }, reason => {
     // reason.error
