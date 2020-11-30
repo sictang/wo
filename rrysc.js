421eeba7d76d20de056abfa0dfa739485f5b9c1d
sictang <63572138+sictang@users.noreply.github.com>
2020-11-30 11:33:09 +0800

    Update rrysc.js

--- rrysc.js
+++ rrysc.js
@@ -1,20 +1,3 @@
-/*
-
-^https:\/\/www\.rrys2020\.com\/User\/login\/ajaxLogin url script-request-header https://raw.githubusercontent.com/sictang/wo/main/rrysc.js
-*/
-
-function getkey(key){
-    for(let i=0 ; i<key.length ; i++){
-        if(key[i] ===';'){
-           const c = key.slice(0,i)
-           return c
-           break
-        }
-        
-    }
-   
-}
-
 const GINFO = response.headers["cookie"][2]
 const GKEY = response.headers["cookie"][3]
 console.log(GINFO);
