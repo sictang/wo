09bb5bc013e797fea54ef04674f60bd779146ed8
sictang <63572138+sictang@users.noreply.github.com>
2020-12-03 12:10:32 +0800

    Update airportc.js

--- airportc.js
+++ airportc.js
@@ -1,21 +1,25 @@
-if ($request.headers) {
-  const airportCookie = 'airportCookie'
-  const header = $request.headers
-  const regtx = /key=.+?;/
+// if ($request.headers) {
+//   const airportCookie = 'airportCookie'
+//   const header = $request.headers
+//   const regtx = /key=.+?;/
  
-  if (header['Cookie'] != undefined) {
+//   if (header['Cookie'] != undefined) {
 
-    if (regtx.exec($prefs.valueForKey(airportCookie))[0] != regtx.exec(header['Cookie'])[0]) {
-      const cookies = header['cookie'].match(/email=.+?\;|ip=.+?\;|key=.+?\;|uid=.+?\;/g)
-      const cookie = cookies[0]+cookies[1]+cookies[2]+cookies[3]
-      $prefs.setValueForKey(cookie, airportCookie)
+//     if ($prefs.valueForKey(airportCookie) {
+//       const cookies = header['cookie'].match(/email=.+?\;|ip=.+?\;|key=.+?\;|uid=.+?\;/g)
+//       const cookie = cookies[0]+cookies[1]+cookies[2]+cookies[3]
+//       $prefs.setValueForKey(cookie, airportCookie)
 
       
-      $notify("", '', 'cookie更新成功')
-    } $notify("", '', 'cookie无需要更新')
-    console.log($prefs.valueForKey(airportCookie))
-    $done()
-  }
-  $done()
+//       $notify("", '', 'cookie更新成功')
+//     }else{
+//       $notify("", '', 'cookie无需要更新')
+//       console.log($prefs.valueForKey(airportCookie))
+//       $done()
+//          }
+//   }
+//   $done()
 
-}
+// }
+console.log($prefs.valueForKey(airportCookie))
+$done()
