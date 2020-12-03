71e42547b856526817087bc2285ed836887797da
sictang <63572138+sictang@users.noreply.github.com>
2020-12-03 10:55:54 +0800

    Update airportc.js

--- airportc.js
+++ airportc.js
@@ -1,2 +1,23 @@
-console.log($request.headers)
-$done()
+if ($request.headers) {
+  const airportKey = 'airportKey'
+  const airportIp = 'airportIp'
+  const header = $request.headers
+  const regtx1 = /key=.+?;/
+  const regtx2 = /ip=.+?;/
+  if (regtx1.exec(header['Cookie']) != undefined) {
+
+    if ($prefs.valueForKey(airportKey) != regtx1.exec(header['Cookie'])[0]) {
+      const key = regtx1.exec(header['Cookie'])[0]
+      const ip = regtx2.exec(header['Cookie'])[0]
+
+      $prefs.setValueForKey(key, airportKey)
+      $prefs.setValueForKey(ip, airportIp)
+      console.log($prefs.valueForKey(key,ip))
+      $notify("", '', 'cookie更新成功')
+    } $notify("", '', 'cookie无需要更新')
+
+    $done()
+  }
+  $done()
+  //重写后加done()网页才会继续打开
+}
