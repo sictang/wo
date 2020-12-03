4d9e5827ac8febce12eb3c7ea65a8b1e64a8d59c
sictang <63572138+sictang@users.noreply.github.com>
2020-12-03 13:31:39 +0800

    Update airportc.js

--- airportc.js
+++ airportc.js
@@ -1,29 +1,24 @@
-
 if ($request.headers) {
   const airportCookie = 'airportCookie'
-  const header = $request.headers
-  const regtx = /key=.+?;/
- 
-  try{
-    if (regtx.exec($prefs.valueForKey(airportCookie))[0] != regtx.exec(header['Cookie'])[0]) {
-      const cookies = header['cookie'].match(/email=.+?\;|ip=.+?\;|key=.+?\;|uid=.+?\;/g)
-      const cookie = cookies[0]+cookies[1]+cookies[2]+cookies[3]
-      $prefs.setValueForKey(cookie, airportCookie)
-      $notify("", '', 'cookie更新成功')
-    } else{
-      $notify("", '', 'cookie无需要更新')
-      console.log($prefs.valueForKey(airportCookie))
+  const nowcookie = $request.headers['cookie']
+  
+    if ($prefs.valueForKey(airportCookie)){
+      if($prefs.valueForKey(airportCookie) != nowcookie){
+        $prefs.setValueForKey(nowcookie,airportCookie)
+        console.log($prefs.valueForKey(airportCookie))
+        $notify("", '', 'cookie更新成功')
+        $done()
+      }else{
+        $notify("", '', 'cookie无需成功')
+        $done()
+      }
+    }else{
+      $prefs.setValueForKey(nowcookie,airportCookie)
+      $notify("", '', 'cookie首次写入成功')
+      $done()
     }
     $done()
-  }
-  catch(err){
-    const cookies = header['cookie'].match(/email=.+?\;|ip=.+?\;|key=.+?\;|uid=.+?\;/g)
-    const cookie = cookies[0]+cookies[1]+cookies[2]+cookies[3]
-    $prefs.setValueForKey(cookie, airportCookie)
-    $notify("", '', 'cookie写入成功')
-    $done()
-  }
-  $done()
-
 }
+
+$done()
 
