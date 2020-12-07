5759413336d8f6cfe7231b55c72dde3f4618cd19
sictang <63572138+sictang@users.noreply.github.com>
2020-12-07 14:59:34 +0800

    Create mc.js

---
+++ mc.js
@@ -0,0 +1,29 @@
+const urlkey = "urlkey"
+const heanderKey = "heanderKey"
+const bodyKey = "bodyKey"
+const theUrlKey = $request.urlkey
+const theHeanderKey = $request.heanders
+const thenBodyKey = $request.bodyKey
+const set = $prefs.setValueForKey
+const get = $prefs.valueForKey
+
+if (get(heanderKey)) {
+    if(get(heanderKey) != get(heanderKey)){
+        set(theUrlKey, urlkey)
+        set(theHeanderKey, heanderKey)
+        set(thenBodyKey, bodyKey)
+        $notify("", '', 'cookie更新成功')
+        $done()
+    }else{
+        $notify("", '', 'cookie无需更新')
+        $done()
+    }
+}else{
+    set(theUrlKey, urlkey)
+    set(theHeanderKey, heanderKey)
+    set(thenBodyKey, bodyKey)
+    $notify("", '', 'cookie首次写入成功')
+    $done()
+}
+
+$done()
