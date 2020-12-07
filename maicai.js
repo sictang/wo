fab8ab8b45ad3d3ac0802a2c41993b128df02940
sictang <63572138+sictang@users.noreply.github.com>
2020-12-07 18:42:00 +0800

    Update maicai.js

--- maicai.js
+++ maicai.js
@@ -1,56 +1,19 @@
+
 const urlKey = "urlKey"
 const headerKey = "headerKey"
 const bodyKey = "bodyKey"
-const theUrlKey = $request.url
-const theHeaderKey = JSON.stringify($request.headers)
-const theBodyKey = $request.body
-set = (key, val) => {return $prefs.setValueForKey(key, val)}
-get = (val => $prefs.valueForKey(val))
-
-
-try{
-    if(typeof $request != "undefined"){
-    getCookie()
-    }
-}catch{
-    checkin()
+get=(val => $prefs.valueForKey(val))
 }
 
-function getCookie(){
-    if (get(headerKey)) {
-        if (get(headerKey) != get(headerKey)) {
-            set(theUrlKey, urlKey)
-            set(theHeaderKey, headerKey)
-            set(theBodyKey, bodyKey)
-            $notify("", '', 'cookie更新成功')
-            $done()
-        } else {
-            console.log('cookie无需更新')
-            $done()
-        }
-    } else {
-        set(theUrlKey, urlKey)
-        set(theHeaderKey, headerKey)
-        set(theBodyKey, bodyKey)
-        $notify("", '', 'cookie首次写入成功')
-        $done()
-    }
+const myContent = {
+    method: 'POST',
+    url:get(urlKey),
+    headers:JSON.parse(get(headerKey)),
+    body:get(bodyKey)
 }
 
-function checkin(){
-    const myContent = {
-        method: 'POST',
-        url: get(urlKey),
-        headers: JSON.parse(get(headerKey)),
-        body: get(bodyKey)
-    }
-
-    $task.fetch(myContent).then(response => {
-        const body = response.body
-        console.log(body)
-    })
-    $done()
-}
-
-
+$task.fetch(myContent).then(response => {
+    const body = response.body
+    console.log(body)
+})
 $done()
