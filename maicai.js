a2bba18441c85ae74033202db437706daa071003
sictang <63572138+sictang@users.noreply.github.com>
2020-12-07 16:54:01 +0800

    Create maicai.js

---
+++ maicai.js
@@ -0,0 +1,20 @@
+const urlKey = "urlKey"
+const headerKey = "headerKey"
+const bodyKey = "bodyKey"
+
+
+get=(val) =>{
+    return $prefs.valueForKey(val)
+}
+
+const myContent = {
+    url:get(urlKey),
+    headers:JSON.parse(get(headerKey)),
+    body:get(bodyKey)
+}
+
+$task.fetch(myContent).then(response => {
+    const body = response.body
+    console.log(body)
+})
+$done()
