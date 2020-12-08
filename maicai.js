be0863a33a70f90b892234a0343b26ca91dc6dc4
sictang <63572138+sictang@users.noreply.github.com>
2020-12-08 22:59:07 +0800

    Update maicai.js

--- maicai.js
+++ maicai.js
@@ -15,14 +15,5 @@ $task.fetch(myContent).then(response => {
     const body = response.body
     console.log(body)
 })
-const myShare ={
-    method:'GET',
-    url:'https://mall.meituan.com/api/c/mallcoin/checkIn/getShareReward?app_tag=union&bizId=2&poi=311&poiId=311&shareBusinessType=2&stockPois=311&tenantId=1&userId=90444748&utm_medium=iphone&utm_term=5.12.0',
-    headers:JSON.parse(get(headerKey))
-}
-$task.fetch(myShare).then(response => {
-    const body = response.body
-    console.log(body)
-})
 
 $done()
