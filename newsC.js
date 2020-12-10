410da232ec236044b3cb0d660c38d5d0bbfd8cd2
sictang <63572138+sictang@users.noreply.github.com>
2020-12-10 14:33:58 +0800

    Create newsC.js

---
+++ newsC.js
@@ -0,0 +1,35 @@
+const newsHeader = 'newsHeader'
+const theHeader = JSON.stringify($request.headers)
+
+if(read(newsHeader)){
+    if(!read(newsHeader)){
+        write(theHeader,newsHeader)
+        console.log('cookie更新成功')
+        notify('cookie更新成功')
+    }else{
+        console.log('cookie无需更新')
+        $done()
+    }
+
+}else{
+    write(theHeader,newsHeader)
+    console.log('首次写入cookie成功')
+    notify('首次写入cookie成功')
+}
+
+function write(key,val){
+    return $prefs.setValueForKey(key,val)
+}
+function read(val){
+    return $prefs.valueForKey(val)
+}
+function notify(title,subtitle,text){
+    $notify(title,subtitle,text)
+    if(subtitle == undefined){
+        subtitle =''
+        text = ''
+    }else if(text == undefined){
+        text = ''
+    }
+    $done()
+}
