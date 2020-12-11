dc37e46068685215c4906d7054feb07c8ff03715
sictang <63572138+sictang@users.noreply.github.com>
2020-12-11 10:53:20 +0800

    Update newsC.js

--- newsC.js
+++ newsC.js
@@ -1,9 +1,10 @@
-const newsHeader = 'newsHeader'
-const theHeader = JSON.stringify($request.headers)
+const newsCookie = 'newsCookie'
+const theHeader = $request.headers
 
-if(read(newsHeader)){
-    if(!read(newsHeader)){
-        write(theHeader,newsHeader)
+if(read(newsCookie)){
+    if(!read(newsCookie)){
+        const userU = theHeader['User-U']
+        write(userU,newsCookie)
         console.log('cookie更新成功')
         notify('cookie更新成功')
     }else{
@@ -23,13 +24,13 @@ function write(key,val){
 function read(val){
     return $prefs.valueForKey(val)
 }
-function notify(title,subtitle,text){
+function notify(title,subtitle,text){ 
     if(subtitle == undefined){
         subtitle =''
         text = ''
     }else if(text == undefined){
         text = ''
     }
-     $notify(title,subtitle,text)
+    $notify(title,subtitle,text)
     $done()
 }
